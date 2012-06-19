b5bb85c6baa239747d3b0fc750df5162b619673b
Stuart Powers <stuart.github@gmail.com>
2012-06-19 11:39:28 -0400

    added gists/2952521/virtualenvwrapper_bashrc.sh

---
+++ gists/2952521/virtualenvwrapper_bashrc.sh
@@ -0,0 +1,7 @@
+echo "" >> $HOME/.bashrc
+echo "# begin virtualenvwrapper support" >> $HOME/.bashrc
+echo "export WORKON_HOME=$HOME/.environments" >> $HOME/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
+echo "# end virtualenvwrapper support" >> $HOME/.bashrc
+echo "" >> $HOME/.bashrc
+
